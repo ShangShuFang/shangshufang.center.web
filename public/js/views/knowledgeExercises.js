ab25953414d065c48c2996f85396482648aefb5b
zhangqiang <johnny.q.zhang@outlook.com>
2020-08-02 11:01:55 +0800

    随堂习题查询条件不使用阶段查询

--- public/js/views/knowledgeExercises.js
+++ public/js/views/knowledgeExercises.js
@@ -1,140 +1,140 @@
 let pageApp = angular.module('pageApp', []);
-pageApp.controller('pageCtrl', function ($scope, $http) {
-  $scope.model = {
-    //begin: 信息编辑
-    technologyID: 0,
-    technologyName: '',
-    learningPhaseID: 0,
-    learningPhaseName: '',
-    knowledgeID: 0,
-    knowledgeName: '',
+pageApp.controller('pageCtrl', function($scope, $http) {
+    $scope.model = {
+        //begin: 信息编辑
+        technologyID: 0,
+        technologyName: '',
+        learningPhaseID: 0,
+        learningPhaseName: '',
+        knowledgeID: 0,
+        knowledgeName: '',
 
-    documentList: [],
-    documentUrl: '',
-    answerGitUrl: '',
-    loginUser: commonUtility.getLoginUser(),
-    //end: 信息编辑
-  };
+        documentList: [],
+        documentUrl: '',
+        answerGitUrl: '',
+        loginUser: commonUtility.getLoginUser(),
+        //end: 信息编辑
+    };
 
-  //region 页面初始化
-  $scope.initPage = function () {
-    commonUtility.setNavActive();
-    if(!$scope.getParameter()){
-      bootbox.alert(localMessage.PARAMETER_ERROR);
-      return false;
-    }
-    $scope.initUploadPlugin();
-    $scope.loadFileList();
-  };
+    //region 页面初始化
+    $scope.initPage = function() {
+        commonUtility.setNavActive();
+        if (!$scope.getParameter()) {
+            bootbox.alert(localMessage.PARAMETER_ERROR);
+            return false;
+        }
+        $scope.initUploadPlugin();
+        $scope.loadFileList();
+    };
 
-  $scope.getParameter = function() {
-    let parameterJson = localStorage.getItem(Constants.KEY_UPLOAD_EXERCISES);
-    if(parameterJson === null) {
-      return false;
-    }
+    $scope.getParameter = function() {
+        let parameterJson = localStorage.getItem(Constants.KEY_UPLOAD_EXERCISES);
+        if (parameterJson === null) {
+            return false;
+        }
 
-    let parameter = JSON.parse(parameterJson);
-    $scope.model.technologyID = parameter.technologyID;
-    $scope.model.technologyName = parameter.technologyName;
-    $scope.model.learningPhaseID = parameter.learningPhaseID;
-    $scope.model.learningPhaseName = parameter.learningPhaseName;
-    $scope.model.knowledgeID = parameter.knowledgeID;
-    $scope.model.knowledgeName = parameter.knowledgeName;
-    return true;
-  };
+        let parameter = JSON.parse(parameterJson);
+        $scope.model.technologyID = parameter.technologyID;
+        $scope.model.technologyName = parameter.technologyName;
+        $scope.model.learningPhaseID = parameter.learningPhaseID;
+        $scope.model.learningPhaseName = parameter.learningPhaseName;
+        $scope.model.knowledgeID = parameter.knowledgeID;
+        $scope.model.knowledgeName = parameter.knowledgeName;
+        return true;
+    };
 
-  $scope.initUploadPlugin = function(){
-    let uploadDocumentDir = {"dir1": "exercises", "dir2": `T${$scope.model.technologyID}`, "dir3": `K${$scope.model.knowledgeID}`};
-    let uploadDocumentServerUrl = commonUtility.buildSystemRemoteUri(Constants.UPLOAD_SERVICE_URI, uploadDocumentDir);
-    uploadUtils.initUploadPlugin('#file-upload-document', uploadDocumentServerUrl, ['pdf'], false, function (opt,data) {
-      $scope.model.documentUrl = data.fileUrlList[0];
-      $scope.$apply();
-      layer.msg(localMessage.UPLOAD_SUCCESS);
-    });
-  };
+    $scope.initUploadPlugin = function() {
+        let uploadDocumentDir = { "dir1": "exercises", "dir2": `T${$scope.model.technologyID}`, "dir3": `K${$scope.model.knowledgeID}` };
+        let uploadDocumentServerUrl = commonUtility.buildSystemRemoteUri(Constants.UPLOAD_SERVICE_URI, uploadDocumentDir);
+        uploadUtils.initUploadPlugin('#file-upload-document', uploadDocumentServerUrl, ['pdf'], false, function(opt, data) {
+            $scope.model.documentUrl = data.fileUrlList[0];
+            $scope.$apply();
+            layer.msg(localMessage.UPLOAD_SUCCESS);
+        });
+    };
 
-  $scope.onChangeDocumentList = function() {
-    $scope.model.documentList.push({
-      documentUrl: $scope.model.documentUrl,
-      documentName: $scope.model.documentUrl.substr($scope.model.documentUrl.lastIndexOf('/') + 1),
-      answerGitUrl: $scope.model.answerGitUrl,
-    });
-    $('#kt_modal_document').modal('hide');
-  };
+    $scope.onChangeDocumentList = function() {
+        $scope.model.documentList.push({
+            documentUrl: $scope.model.documentUrl,
+            documentName: $scope.model.documentUrl.substr($scope.model.documentUrl.lastIndexOf('/') + 1),
+            answerGitUrl: $scope.model.answerGitUrl,
+        });
+        $('#kt_modal_document').modal('hide');
+    };
 
-  $scope.loadFileList = function(){
-    $http.get(`/knowledge/exercises/files?technologyID=${$scope.model.technologyID}&learningPhaseID=${$scope.model.learningPhaseID}&knowledgeID=${$scope.model.knowledgeID}`).then(function successCallback (response) {
-      if(response.data.err){
-        bootbox.alert(localMessage.formatMessage(response.data.code, response.data.msg));
-        return false;
-      }
-      if(response.data.files === null){
-        return false;
-      }
+    $scope.loadFileList = function() {
+        $http.get(`/knowledge/exercises/files?technologyID=${$scope.model.technologyID}&knowledgeID=${$scope.model.knowledgeID}`).then(function successCallback(response) {
+            if (response.data.err) {
+                bootbox.alert(localMessage.formatMessage(response.data.code, response.data.msg));
+                return false;
+            }
+            if (response.data.files === null) {
+                return false;
+            }
 
-      $scope.model.documentList.splice(0, response.data.files.length);
-      if(response.data.files !== null){
-        response.data.files.forEach(function (file) {
-          $scope.model.documentList.push({
-            documentUrl: file.documentUrl,
-            documentName: file.documentUrl.substr(file.documentUrl.lastIndexOf('/') + 1),
-            answerGitUrl: file.answerUrl,
-            dataStatus: file.dataStatus,
-            dataStatusText: file.dataStatusText,
-            createUser: file.createUser,
-            createTime: file.createTime
-          });
+            $scope.model.documentList.splice(0, response.data.files.length);
+            if (response.data.files !== null) {
+                response.data.files.forEach(function(file) {
+                    $scope.model.documentList.push({
+                        documentUrl: file.documentUrl,
+                        documentName: file.documentUrl.substr(file.documentUrl.lastIndexOf('/') + 1),
+                        answerGitUrl: file.answerUrl,
+                        dataStatus: file.dataStatus,
+                        dataStatusText: file.dataStatusText,
+                        createUser: file.createUser,
+                        createTime: file.createTime
+                    });
+                })
+            }
+        }, function errorCallback(response) {
+            bootbox.alert(localMessage.NETWORK_ERROR);
+        });
+    };
+
+    $scope.onShowUploadDocumentModal = function() {
+        $('div.cleanFileBt').trigger("click");
+        $scope.model.answerGitUrl = '';
+        $scope.model.documentUrl = '';
+        $('#kt_modal_document').modal('show');
+    };
+
+    $scope.onRemoveDocument = function(data) {
+        $scope.model.documentList.forEach(function(image, index) {
+            if (image.documentUrl === data.documentUrl) {
+                $scope.model.documentList.splice(index, 1);
+            }
         })
-      }
-    }, function errorCallback(response) {
-        bootbox.alert(localMessage.NETWORK_ERROR);
-    });
-  };
+    };
 
-  $scope.onShowUploadDocumentModal = function(){
-    $('div.cleanFileBt').trigger("click");
-    $scope.model.answerGitUrl = '';
-    $scope.model.documentUrl = '';
-    $('#kt_modal_document').modal('show');
-  };
+    $scope.onSave = function() {
+        let documentObjectArray = [];
 
-  $scope.onRemoveDocument = function(data){
-    $scope.model.documentList.forEach(function (image, index) {
-      if(image.documentUrl === data.documentUrl){
-        $scope.model.documentList.splice(index, 1);
-      }
-    })
-  };
+        $scope.model.documentList.forEach(function(document) {
+            documentObjectArray.push({
+                documentUrl: document.documentUrl,
+                answerUrl: document.answerGitUrl
+            });
+        });
+        $http.post('/knowledge/exercises', {
+            technologyID: $scope.model.technologyID,
+            learningPhaseID: $scope.model.learningPhaseID,
+            knowledgeID: $scope.model.knowledgeID,
+            exercisesJson: JSON.stringify(documentObjectArray),
+            loginUser: $scope.model.loginUser.adminID
+        }).then(function successCallback(response) {
+            if (response.data.err) {
+                bootbox.alert(localMessage.formatMessage(response.data.code, response.data.msg));
+                return false;
+            }
+            $scope.loadFileList();
+            layer.msg(localMessage.UPLOAD_SUCCESS);
+        }, function errorCallback(response) {
+            bootbox.alert(localMessage.NETWORK_ERROR);
+        });
+    };
+    //endregion
 
-  $scope.onSave = function(){
-    let documentObjectArray = [];
-
-    $scope.model.documentList.forEach(function (document) {
-      documentObjectArray.push({
-        documentUrl: document.documentUrl,
-        answerUrl: document.answerGitUrl
-      });
-    });
-    $http.post('/knowledge/exercises', {
-      technologyID: $scope.model.technologyID,
-      learningPhaseID: $scope.model.learningPhaseID,
-      knowledgeID: $scope.model.knowledgeID,
-      exercisesJson: JSON.stringify(documentObjectArray),
-      loginUser: $scope.model.loginUser.adminID
-    }).then(function successCallback(response) {
-      if(response.data.err){
-        bootbox.alert(localMessage.formatMessage(response.data.code, response.data.msg));
-        return false;
-      }
-      $scope.loadFileList();
-      layer.msg(localMessage.UPLOAD_SUCCESS);
-    }, function errorCallback(response) {
-      bootbox.alert(localMessage.NETWORK_ERROR);
-    });
-  };
-  //endregion
-
-  $scope.initPage();
+    $scope.initPage();
 });
 
 angular.bootstrap(document.querySelector('[ng-app="pageApp"]'), ['pageApp']);
--- routes/knowledgeExercises.js
+++ routes/knowledgeExercises.js
@@ -3,59 +3,58 @@ let router = express.Router();
 let commonService = require('../service/commonService');
 
 router.get('/', function(req, res, next) {
-  res.render('knowledgeExercises', {title: '习题上传'});
+    res.render('knowledgeExercises', { title: '习题上传' });
 });
 
 router.get('/files', function(req, res, next) {
-  let service = new commonService.commonInvoke('knowledgeExercisesList');
-  let technologyID = req.query.technologyID;
-  let learningPhaseID = req.query.learningPhaseID;
-  let knowledgeID = req.query.knowledgeID;
-  let parameter = `1/999/${technologyID}/${learningPhaseID}/${knowledgeID}`;
+    let service = new commonService.commonInvoke('knowledgeExercisesList');
+    let technologyID = req.query.technologyID;
+    let knowledgeID = req.query.knowledgeID;
+    let parameter = `1/999/${technologyID}/${knowledgeID}`;
 
-  service.queryWithParameter(parameter, function (result) {
-    if(result.err){
-      res.json({
-        err: true,
-        code: result.code,
-        msg: result.msg
-      });
-    }else{
-      res.json({
-        err: false,
-        code: result.code,
-        msg: result.msg,
-        files: result.content.responseData
-      });
-    }
-  });
+    service.queryWithParameter(parameter, function(result) {
+        if (result.err) {
+            res.json({
+                err: true,
+                code: result.code,
+                msg: result.msg
+            });
+        } else {
+            res.json({
+                err: false,
+                code: result.code,
+                msg: result.msg,
+                files: result.content.responseData
+            });
+        }
+    });
 });
 
 router.post('/', (req, res, next) => {
-  let service = new commonService.commonInvoke('addKnowledgeExercises');
-  let data = {
-    technologyID: req.body.technologyID,
-    learningPhaseID: req.body.learningPhaseID,
-    knowledgeID: req.body.knowledgeID,
-    exercisesJson: req.body.exercisesJson,
-    loginUser: req.body.loginUser
-  };
+    let service = new commonService.commonInvoke('addKnowledgeExercises');
+    let data = {
+        technologyID: req.body.technologyID,
+        learningPhaseID: req.body.learningPhaseID,
+        knowledgeID: req.body.knowledgeID,
+        exercisesJson: req.body.exercisesJson,
+        loginUser: req.body.loginUser
+    };
 
-  service.create(data, (result) => {
-    if(result.err){
-      res.json({
-        err: true,
-        code: result.code,
-        msg: result.msg
-      });
-    }else{
-      res.json({
-        err: false,
-        code: result.code,
-        msg: result.msg
-      });
-    }
-  });
+    service.create(data, (result) => {
+        if (result.err) {
+            res.json({
+                err: true,
+                code: result.code,
+                msg: result.msg
+            });
+        } else {
+            res.json({
+                err: false,
+                code: result.code,
+                msg: result.msg
+            });
+        }
+    });
 });
 
 module.exports = router;
